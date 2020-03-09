25d6962d58c170cd6ed37737dfcfb25043f2bda1
Chip Mandal <cmandal@apple.com>
2020-03-08 21:33:56 -0700

    text_to_keywords implementation - (incomplete)

--- text_to_keywords/train.py
+++ text_to_keywords/train.py
@@ -18,8 +18,8 @@ import torch.optim as optim
 from tqdm import trange
 
 import utils
-from model import net
-from model.data_loader import DataLoader
+from model2 import net
+from model2.data_loader import DataLoader
 from evaluate import evaluate
 
 
